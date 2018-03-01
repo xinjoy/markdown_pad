bcb465f4f18a9f5eaecb67b8d22b836f0d7aaeb1
Xin Xin <xinjoy73@gmail.com>
2018-03-01 02:06:20 -0800

    load defaul md fix

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -5,6 +5,72 @@ import Toolbar from '../../components/Toolbar/Toolbar';
 
 import axios from 'axios';
 
+const initialInput = `# Markdown Pad
+
+[Markdown Pad](https://github.com/xxin666/markdown_pad) is a light, fast, React powered online Markdown editor.
+
+## New Features
+  - Create new Markdown
+  - Export documents as Markdown and HTML
+
+## Dynamic Preivew
+  - Automatically rendered as you type
+  - Type Markdown on the left pane
+  - Find Magic in the right
+
+## Syntax Highlighting
+\`\`\`js
+var appName = "Markdown Pad";
+
+function sayHello() {
+    console.log("Hello from Markdown Pad");
+}
+\`\`\`
+
+## Tables
+
+Colons can be used to align columns.
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+## Blockquotes
+
+> Blockquotes are very handy in email to emulate reply text.
+
+> This line is part of the same quote.
+
+## Todos
+
+ - Import and Export files
+
+## Development
+\`\`\`bash
+$ git clone https://github.com/xxin666/markdown_pad.git
+$ cd markdown_pad
+$ npm install
+\`\`\`
+
+## Run
+\`\`\`bash
+$ npm start
+\`\`\`
+
+## Build
+\`\`\`bash
+$ npm build
+\`\`\`
+
+## Acknowledgment
+Icon made by [Revicon](https://www.flaticon.com/authors/revicon) from [www.flaticon.com](www.flaticon.com)
+
+License
+----
+MIT`;
+
 class Layout extends Component {
     constructor(props) {
         super(props);
@@ -24,11 +90,8 @@ class Layout extends Component {
             .then(response => {
                 this.setState({ markdownInput: response.data.replace(/\\/g, '')});
             })
-            .catch(error => {
-                axios.get('http://localhost:3000/static/media/default.2fbc3786.md')
-                    .then(res => {
-                        this.setState({ markdownInput: res.data.replace(/\\/g, '')});
-                    })
+            .catch(error => {   
+                this.setState({ markdownInput: initialInput});
             });
     }
 
